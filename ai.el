921b235211895349cfaa1382ac082e5de262c0e6
Alexey Shumitsky <alexey.shumitsky@gmail.com>
2025-04-14 22:14:33 +0200

    Add openrouter connection

--- ai.el
+++ ai.el
@@ -1,6 +1,7 @@
 
 (setq secret-token-openai (or (secrets-get-secret "Login" "openai-home-key") ""))
 (setq secret-token-gemini (or (secrets-get-secret "Login" "gemini-api-key") ""))
+(setq secret-token-openrouter (or (secrets-get-secret "Login" "openrouter-aider") ""))
 
 (use-package aidermacs
   :ensure t
@@ -9,8 +10,9 @@
   (setenv "OPENAI_API_KEY" secret-token-openai)
   (setenv "GEMINI_API_KEY" secret-token-gemini)
   (setenv "OLLAMA_API_BASE" "http://127.0.0.1:11434")
+  (setenv "OPENROUTER_API_KEY" secret-token-openrouter)
   :custom
   (aidermacs-use-architect-mode t)
-  (aidermacs-architect-model "gemini/gemini-2.0-pro-exp-02-05")
-  (aidermacs-editor-model "gemini/gemini-2.0-flash-exp")
+  (aidermacs-architect-model "openrouter/google/gemini-2.5-pro-preview-03-25")
+  (aidermacs-editor-model "openrouter/google/gemini-2.0-flash-001")
   )
